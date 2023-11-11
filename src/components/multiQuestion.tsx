9bb2b37444a0979a1ac14bc4ce8322ee07f54659
Jack Rabe <jhrabe@wisc.edu>
2023-11-11 16:52:08 -0600

    multi-select screen

--- src/components/multiQuestion.tsx
+++ src/components/multiQuestion.tsx
@@ -10,26 +10,42 @@ export default function MultiQuestion({ question }: Props) {
   const [selected, setSelected] = useState<string[]>([]);
 
   function handleSelection() {
-    selected.reduce((acc, el) => acc + " " + el, "");
-    alert("selected" + selected);
+    const s = selected.reduce((acc, el) => acc + " " + el, "");
+    alert("selected: " + s);
+    window.location.href = "/";
   }
 
   return (
-    <>
+    <div className="flex items-center flex-col">
       {question.availableFields?.map((field) => {
         return (
-          <div key={field} className="flex items-center space-x-2 m-4">
-            <Checkbox id="terms" />
+          <div key={field} className="flex items-center space-x-2 m-8">
+            <Checkbox
+              className="text-3xl"
+              id={field}
+              onCheckedChange={(change) => {
+                if (selected.includes(field)) {
+                  setSelected((prev) => {
+                    const copy = prev.slice();
+                    return copy.filter((el) => el !== field);
+                  });
+                } else {
+                  selected.push(field);
+                }
+              }}
+            />
             <label
-              htmlFor="terms"
-              className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              htmlFor={field}
+              className="text-3xl font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
             >
               {field}
             </label>
           </div>
         );
       })}
-      <Button>Submit</Button>
-    </>
+      <Button className="text-xl" onClick={handleSelection}>
+        Submit
+      </Button>
+    </div>
   );
 }
