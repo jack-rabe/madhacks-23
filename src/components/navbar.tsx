9bb00a05494607980298a763d5803d6a77a77d42
Jack Rabe <jhrabe@wisc.edu>
2023-11-12 10:33:07 -0600

    modify nav, disable eslint rules

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,26 +11,10 @@ export default function Navbar() {
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
             <li>
               <a
-                href="#"
+                href="https://github.com/jack-rabe/madhacks-23"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
               >
-                Learn
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-              >
-                Leaderboard?
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-              >
-                Other...
+                Contribute
               </a>
             </li>
           </ul>
