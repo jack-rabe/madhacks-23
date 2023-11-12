2cbfe3bf3c5f67fbaf0dec351cd4dc73753a7ee9
Jack Rabe <jhrabe@wisc.edu>
2023-11-12 10:02:41 -0600

    Revert "Revert "styling changes""
    This reverts commit cd893e3195aa9e3a81bd1b03d0d5bf59827bb5d5.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="bg-white border-gray-200 border">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap">
-            Opeify
-          </span>
+          <Link href="/">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap">
+              Opeify
+            </span>
+          </Link>
         </a>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
