413813b7d457dfe54288fe2f9c79223e1ffa5e18
Jack Rabe <jhrabe@wisc.edu>
2023-11-11 23:18:44 -0600

    show check on finished questions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Card } from "@/components/ui/card";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Question } from "@/lib/utils";
 import { useAuth0 } from "@auth0/auth0-react";
+import { CheckIcon } from "@radix-ui/react-icons";
 
 export default function Home() {
   let [lessons, setLessons] = useState<Question[]>([]);
@@ -31,12 +32,12 @@ export default function Home() {
     <>
       <div>
         <h2 className="p-3 m-4 text-2xl">Questions</h2>
-        <div className="grid grid-cols-1">
+        <div className="grid grid-cols-2">
           {lessons.map((lesson) => (
             <Card
               key={lesson.seqNumber}
-              className={`w-1/2 m-2 ${
-                lesson.seqNumber > score ? "bg-red-200" : ""
+              className={`w-5/6 m-2 ${
+                lesson.seqNumber > score ? "bg-slate-300" : ""
               }`}
             >
               <Link
@@ -47,12 +48,19 @@ export default function Home() {
                 }}
               >
                 <div className="text-2xl p-4">{lesson.category}</div>
-                <Button
-                  className="text-2xl p-4 mr-4"
-                  disabled={lesson.seqNumber > score}
-                >
-                  Learn
-                </Button>
+                {lesson.seqNumber == score && (
+                  <Button
+                    className="text-2xl p-4 mr-4"
+                    disabled={lesson.seqNumber != score}
+                  >
+                    Learn
+                  </Button>
+                )}
+                {lesson.seqNumber < score && (
+                  <div className="bg-green-400 p-2 mr-4">
+                    <CheckIcon />
+                  </div>
+                )}
               </Link>
             </Card>
           ))}
