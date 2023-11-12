f89bfa89f5934f579f90607907126c3eaddaa67b
Ben Wurster <wursterben17@gmail.com>
2023-11-12 10:55:26 -0600

    database fix

--- src/components/speechQuestion.tsx
+++ src/components/speechQuestion.tsx
@@ -20,62 +20,68 @@ export default function SpeechQuestion({ question }: Props) {
         const recorder = new MediaRecorder(stream);
         recorder.ondataavailable = (e) => {
           if (e.data.size > 0) {
-            setAudioChunks([e.data]);
+            setAudioChunks((prev) => [...prev, e.data]);
           }
         };
         recorder.onstop = () => {
           const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
-          const formData = new FormData();
-          formData.append('audio', audioBlob, 'recorded_audio.wav');
-
-          console.log("Sup mafk");
-          // fetch('your_upload_url', {
-          //     method: 'POST',
-          //     body: formData
-          // })
-          // .then(response => response.json())
-          // .then(data => {
-          //     console.log('Upload successful:', data);
-          // })
-          // .catch(error => {
-          //     console.error('Error uploading audio:', error);
-          // });
           const url = URL.createObjectURL(audioBlob);
           setAudioUrl(url);
         };
         setMediaRecorder(recorder);
-        console.log(mediaRecorder);
-        setRecording(true);
         recorder.start();
-        setTimeout(stopRecording, 2000, recorder);
-        // setTimeout(stopRecording.bind(this, recorder), 2000);
+
+        // Stop recording after 5 seconds (adjust the time as needed)
+        setTimeout(() => {
+          if (recorder.state === 'recording') {
+            recorder.stop();
+          }
+        }, 5000);
       })
       .catch((error) => console.error('Error accessing microphone:', error));
+    setTimeout(startRecording, 2000);
   };
 
-  const stopRecording = (recorder : MediaRecorder) => {
-    if (recorder) {
-      setRecording(false);
-      recorder.stop();
+  const stopRecording = () => {
+    if (mediaRecorder && mediaRecorder.state === 'recording') {
+      mediaRecorder.stop();
     }
   };
 
-  return <div>
-      {audioUrl ?
-      <audio id="audioPlayer" src={audioUrl} controls></audio>
-      :
-      <audio id="audioPlayer" controls></audio>
-      }
-      {recording ?
-        (<div className="flex justify-center flex-col items-center">
-          <p>Speak Now</p>
-          <Button onClick={startRecording} variant="outline" size="icon" disabled><FontAwesomeIcon icon={faMicrophone} /></Button>
-        </div>)
-        :
-        (<div className="flex justify-center flex-col items-center">
-          <p>Click to Record</p>
-          <Button onClick={startRecording} size="icon"><FontAwesomeIcon icon={faMicrophone} /></Button>
-        </div>)
-      }
+  const playAudio = () => {
+    if (audioUrl) {
+      const audio = new Audio(audioUrl);
+      audio.play();
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
+      <button onClick={playAudio} disabled={!audioUrl}>
+        Play Audio
+      </button>
+      {audioUrl && <audio controls src={audioUrl} />}
     </div>
-}
+  );
+};
+
+  // return <div>
+  //     {audioUrl ?
+  //     <audio id="audioPlayer" src={audioUrl} controls></audio>
+  //     :
+  //     <audio id="audioPlayer" controls></audio>
+  //     }
+  //     {recording ?
+  //       (<div className="flex justify-center flex-col items-center">
+  //         <p>Speak Now</p>
+  //         <Button id="startButton" variant="outline" size="icon" disabled><FontAwesomeIcon icon={faMicrophone} /></Button>
+  //       </div>)
+  //       :
+  //       (<div className="flex justify-center flex-col items-center">
+  //         <p>Click to Record</p>
+  //         <Button id="startButton" size="icon"><FontAwesomeIcon icon={faMicrophone} /></Button>
+  //       </div>)
+  //     }
+  //   </div>
